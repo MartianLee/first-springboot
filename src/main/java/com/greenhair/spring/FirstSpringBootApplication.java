3eef8fc8d60d7db927bb1b3309a0f5797ccb095e
MartianLee <martionlee@gmail.com>
2019-03-17 14:25:25 +0900

    init project

---
+++ src/main/java/com/greenhair/spring/FirstSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.greenhair.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootApplication.class, args);
+	}
+
+}
